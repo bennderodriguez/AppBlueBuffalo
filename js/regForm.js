42a262ca0df3c47e1d82e712c3745f7bd195f690
Bernabe Gutierrez <bernita.gutierrez@gmail.com>
2018-04-04 20:20:22 -0500

    Version 0.5

--- js/regForm.js
+++ js/regForm.js
@@ -11,53 +11,106 @@ $("#regForm").validator().on("submit", function (event) {
 });
 
 
-function submitForm(){
+function submitForm() {
+    console.log("llege perro");
+    
     // Initiate Variables With Form Content
+    var Mascota = $("#Mascota").val();
+    var nombre_mascota = $("#nombre_mascota").val();
     var lugar = $("#lugar").val();
-	console.log(lugar);
-	var tipo_mascota = $("#tipo_mascota").val();
-	console.log(tipo_mascota);
-	var nombre = $("#nombre").val();
-	console.log(nombre);
-	var edad_numero = $("#edad_numero").val();
-	console.log(edad_numero);
-	var edad_mm_yy = $("#edad_mm_yy").val();
-	console.log(edad_mm_yy);
-	var nombre_padre = $("#nombre_padre").val();
-	console.log(nombre_padre);
-	var email = $("#email").val();
-	console.log(email);
+    var tamano = $("#tamano").val();
+    var edad_numero = $("#edad_numero").val();
+    var edad_mm_yy = $("#edad_mm_yy").val();
+    var nombre_padre = $("#nombre_padre").val();
+    var email = $("#email").val();
+    var marca = $("#marca").val();
+    var clienteSelecciono = $("#clienteSelecciono").val();
+    var d = new Date();
+    console.log(d.toLocaleString("en-US"));
+    console.log(Mascota);
+    console.log(nombre_mascota);
+    console.log(lugar);
+    console.log(tamano);
+
+    var text = '[{"nombre_mascota":"' + nombre_mascota + '","lugar":"' + lugar + '","tamano":"' + tamano + '","Hora":"' + d.toLocaleString("en-US") + '"}]';
+    clickCounter(text);
+// creas el fichero con la API File
+    var file = new File([text], "encusta.json", {type: "text/plain;charset=utf-8"});
+
+// obtienes una URL para el fichero que acabas de crear
+    var url = window.URL.createObjectURL(file);
+
+// creas un enlace y lo añades al documento
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+
+// actualizas los parámetros del enlace para descargar el fichero creado
+    a.href = url;
+    a.innerHTML = "Descargar fichero";
+    a.download = file.name;
+
+    console.log(file);
+
+//    // Check browser support
+//    if (typeof (Storage) !== "undefined") {
+//        // Store
+//        localStorage.setItem("Respuestas", text);
+//        // Retrieve
+//        document.getElementById("result").innerHTML = localStorage.getItem("Respuestas");
+//    } else {
+//        document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+//    }
+
     /*$.ajax({
-        type: "POST",
-        url: "php/form-process.php",
-        data: "name=" + name + "&email=" + email + "&message=" + message,
-        success : function(text){
-            if (text == "success"){
-                formSuccess();
-            } else {
-                formError();
-                submitMSG(false,text);
-            }
-        }
-    });*/
+     type: "POST",
+     url: "phpp/form-process.php",
+     data: "name=" + name + "&email=" + email + "&message=" + message,
+     success : function(text){
+     if (text == "success"){
+     formSuccess();
+     } else {
+     formError();
+     submitMSG(false,text);
+     }
+     }
+     });*/
+    
+    formSuccess();
 }
 
-function formSuccess(){
+function formSuccess() {
     $("#regForm")[0].reset();
-    submitMSG(true, "Message Submitted!")
+    submitMSG(true, "Respuestas Enviadas!")
+   // $(location).attr('href', 'pr1.html')
 }
 
-function formError(){
-    $("#regForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+function formError() {
+    $("#regForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
         $(this).removeClass();
     });
 }
 
-function submitMSG(valid, msg){
-    if(valid){
+function submitMSG(valid, msg) {
+    if (valid) {
         var msgClasses = "h3 text-center tada animated text-success";
     } else {
         var msgClasses = "h3 text-center text-danger";
     }
     $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+}
+
+function clickCounter(text) {
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount)+1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("result").innerHTML = "Numero de encuestados: " + localStorage.clickcount;
+        console.log("Numero de encuestas Totales: " + localStorage.clickcount);
+        localStorage.setItem("Respuestas", text);
+        console.log(localStorage.getItem("Respuestas"));
+    } else {
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    }
 }
